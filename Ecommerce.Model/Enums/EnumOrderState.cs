732fadefb80286e35cc38a4fcc38801ec1b36081
bilalkirkgul <61188245+bilalkirkgul@users.noreply.github.com>
2021-01-10 20:05:31 +0300

    second commit

--- Ecommerce.Model/Enums/EnumOrderState.cs
+++ Ecommerce.Model/Enums/EnumOrderState.cs
@@ -6,9 +6,9 @@ namespace Ecommerce.Model.Enums
 {
     public enum EnumOrderState
     {
-        waiting = 0,//beklemde
-        Unpaid = 1,//ödeme alınmadı
-        Completed = 2//tamamalandı
+        waiting = 0,
+        Unpaid = 1,
+        Completed = 2
 
     }
 }
